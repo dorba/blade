1ebcd847e9d07bdc133aba2f4264c926fe4cdb48
Barry Dorman <barry.dorman@vannatech.com>
2012-08-14 20:36:14 -0500

    Changes to lambda expression transforms; fix for Issue #14.

--- source/Core/Compiler/Transformation/CSharp/Expression/SimpleLambdaExpression.Transformer.cs
+++ source/Core/Compiler/Transformation/CSharp/Expression/SimpleLambdaExpression.Transformer.cs
@@ -9,8 +9,20 @@ namespace Blade.Compiler.Transformation.CSharp
         public override IEnumerable<IModel> VisitSimpleLambdaExpression(SimpleLambdaExpressionSyntax node)
         {
             var model = Create<LambdaExpression>(node);
-            model.Body = TransformToSingle<ISyntacticModel>(node.Body);
+            model.Body = TransformToSingle<ExpressionModel>(node.Body);
             model.Parameters.Add(TransformToSingle<ParameterDeclaration>(node.Parameter));
+
+            // if the lambda returns a value an the body is an expression,
+            // we should explicitly include the return statement
+            var expr = model.Body as ExpressionModel;
+            if (expr != null)
+            {
+                var info = GetSymbol(node) as MethodSymbol;
+                if (info != null && info.ReturnType.SpecialType != Roslyn.Compilers.SpecialType.System_Void)
+                {
+                    model.Body = new ReturnStatement { Expression = expr };
+                }
+            }
 
             yield return model;
         }
